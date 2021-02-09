e28d1495fcca520168097817357d12c97028df52
LUCAS PEDRONE <lucas.pedrone@matera.com>
2021-02-08 21:08:31 -0300

    Creating Speciality Service Impl. Closes #30

---
+++ pet-clinic-data/src/main/java/springframework/petclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package springframework.petclinic.services;
+
+import springframework.petclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long>{
+}
---
+++ pet-clinic-data/src/main/java/springframework/petclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,35 @@
+package springframework.petclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import springframework.petclinic.model.Speciality;
+import springframework.petclinic.services.SpecialityService;
+
+import java.util.Set;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialityService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
